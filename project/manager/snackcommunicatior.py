cbbcca5b9735f301d43d84a165d8da0e3ff8962a
Luis Gomez <luis15gomez@gmail.com>
2022-01-11 22:04:45 -0600

    Missing self

--- project/manager/snackcommunicatior.py
+++ project/manager/snackcommunicatior.py
@@ -10,11 +10,11 @@ class SnackCommunicatior():
 		ip = 'http://' + self.snack_provider.get_ip_for(mac_address) + '/instruction'
 		if body is not None:
 			request = HTTPRequest(ip, 'POST', parameters = body)
-			json = network_client.request(request)
+			json = self.network_client.request(request)
 		else:
 			print('Nothing to write to snack address: ', str(ip))
 
 	def read(self, mac_address):
 		ip = self.snack_provider.get_ip_for(mac_address)
 		request = HTTPRequest(ip, 'GET')
-		json = network_client.request(request)
+		json = self.network_client.request(request)
