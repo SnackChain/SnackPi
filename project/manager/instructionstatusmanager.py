39012f06d8dd8a2a9f3981e0dcec6c5cc50c2cc2
Luis Gomez <luis15gomez@gmail.com>
2022-01-20 18:37:43 -0600

    Fix current_status

--- project/manager/instructionstatusmanager.py
+++ project/manager/instructionstatusmanager.py
@@ -10,11 +10,11 @@ class InstructionStatusManager():
 		self.required_parameters = required_parameters
 
 	def set_ready_status_if_requirements_met(self, available_snacks):
-		if self.status == self.FIRED:
+		if self.current_status == self.FIRED:
 			return
 		all_addresses_available = self.all_addresses_available(available_snacks)
 		if all_addresses_available:
-			self.status = self.READY
+			self.current_status = self.READY
 
 	def all_addresses_available(self, available_snacks):
 		all_addresses_available = True
@@ -24,7 +24,7 @@ class InstructionStatusManager():
 		return all_addresses_available
 
 	def is_ready_to_fire(self):
-		return self.status == self.READY
+		return self.current_status == self.READY
 
 	def set_fired_status(self):
-		self.status = self.FIRED
+		self.current_status = self.FIRED
