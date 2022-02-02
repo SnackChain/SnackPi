4b3f42b34f6d7c924f856c0a79da7a84dc2fc446
Luis Gomez <luis15gomez@gmail.com>
2022-02-02 17:38:49 -0600

    Fix

--- project/main.py
+++ project/main.py
@@ -51,8 +51,7 @@ class InstructionsHandler():
 		self.instruction_provider = instruction_provider
 
 	def run_pending(self, available_snacks):
-		print(self.instruction_provider.handlers)
-		for key, single_instruction_handler in self.instruction_provider.handlers:
+		for _, single_instruction_handler in self.instruction_provider.handlers.iteritems():
 			single_instruction_handler.set_ready_status_if_requirements_met(available_snacks)
 			single_instruction_handler.fire_instruction_if_ready()
 
